5a88b3f7c017f8ef70db7673d34de5c89c255ac1
Pradyumn Chamoli <pradyumnchamoli18@gmail.com>
2026-05-25 22:43:53 +0530

    Added controller for logout

--- server/controllers/authcontroller.js
+++ server/controllers/authcontroller.js
@@ -33,7 +33,7 @@ export const register = async (req, res) => {
     });
 
     return res.jsom({success:true});
-    
+
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
@@ -79,3 +79,20 @@ export const login = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+
+export const logout = async (req,res) =>{
+    try{
+        res.clearCookie('token',{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",    
+        })
+
+        return res.json({success:true , message:"Logged out"});
+        
+    }catch(error){
+        res.json({success:false , message:error.message});
+        
+    }
+}
