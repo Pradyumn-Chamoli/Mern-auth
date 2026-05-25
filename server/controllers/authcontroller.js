c710f4fa58749797fcc3c3ada850b2b70582b458
Pradyumn Chamoli <pradyumnchamoli18@gmail.com>
2026-05-25 20:43:03 +0530

    created the controller for login

--- server/controllers/authcontroller.js
+++ server/controllers/authcontroller.js
@@ -1,41 +1,81 @@
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import userModel from '../models/userModel.js';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
 
-export const register = async(req,res)=>{
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
 
-    const {name,email,password} = req.body;
+  if (!name || !email || !password) {
+    return res.json({ success: false, message: "Missing Details" });
+  }
 
-    if(!name || !email || !password){
-        return res.json({success:false , message: 'Missing Details'}); 
+  try {
+    const existingUser = await userModel.findOne({ email });
+
+    if (existingUser) {
+      return res.json({ success: false, message: "User already exists" });
     }
 
-    try {
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const existingUser= await userModel.findOne({email});
+    const user = new userModel({ name, email, password: hashedPassword });
+    await user.save();
 
-        if(existingUser){
-            return res.json({success:false , message : "User already exists"});
-        }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
 
-        const hashedPassword = await bcrypt.hash(password,10);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
-        const user = new userModel({name,email,password:hashedPassword});
-        await user.save();
+    return res.jsom({success:true});
+    
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
-        const token = jwt.sign({id: user._id},process.env.JWT_SECRET,{expiresIn : '7d'});
+export const login = async (req, res) => {
+  const { email, password } = req.body;
 
-        res.cookie('token' ,  token , {
-            httpOnly:true,
-            secure:process.env.NODE_ENV ==='production',
-            sameSite:process.env.NODE_ENV ==='production'? 'none' : 'strict',
-            maxAge : 7 * 24 * 60 * 60 * 1000
-        });
+  if (!email || !password) {
+    return res.json({
+      success: false,
+      message: "Email and Pasowrd are required",
+    });
+  }
 
+  try {
+    const user = await userModel.findOne({ email });
 
-        
-    } catch (error) {
-        res.json({success : false , message:error.message});
-        
+    if (!user) {
+      return res.json({ success: false, message: "Invalid Email" });
     }
-}
+
+    const isMatch = await bcrypt.compare(password, user, password);
+
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.jsom({success:true});
+
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
